b218b238539a4a6c21bf67bb91006fb6f2a358d2
suman kharel <39797479+skharel11@users.noreply.github.com>
2018-06-20 11:47:04 +0545

    Create CategoryView.ascx.cs
    Code behind file for CategoryView.ascx file

---
+++ CategoryView.ascx.cs
@@ -0,0 +1,26 @@
+using SageFrame.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Modules_Category_CategoryView : BaseUserControl
+{
+    public int portalID, userModuleID;
+    public string cultureCode, username;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            portalID = GetPortalID;
+            userModuleID = int.Parse(SageUserModuleID);
+            cultureCode = GetCurrentCulture();
+            username = GetUsername;
+
+            IncludeCss("adminCss", "/Administrator/Templates/Default/css/admin.css");
+
+        }
+    }
+}
